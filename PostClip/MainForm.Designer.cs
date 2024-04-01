eee689c2d4e81ed4b56e3b8c04042d4043923ede
pluveto <i@pluvet.com>
2024-04-01 23:02:31 +0800

    Add project files.

---
+++ PostClip/MainForm.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace PostClip
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            this.DisposeCustom(disposing);
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAddRule = new System.Windows.Forms.Button();
+            this.txtPattern = new System.Windows.Forms.TextBox();
+            this.txtReplacement = new System.Windows.Forms.TextBox();
+            this.btnRemoveRule = new System.Windows.Forms.Button();
+            this.lstRules = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnAddRule
+            // 
+            this.btnAddRule.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAddRule.Location = new System.Drawing.Point(1047, 47);
+            this.btnAddRule.Name = "btnAddRule";
+            this.btnAddRule.Size = new System.Drawing.Size(87, 29);
+            this.btnAddRule.TabIndex = 0;
+            this.btnAddRule.Text = "Add Rule";
+            this.btnAddRule.UseVisualStyleBackColor = true;
+            this.btnAddRule.Click += new System.EventHandler(this.btnAddRule_Click);
+            // 
+            // txtPattern
+            // 
+            this.txtPattern.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.txtPattern.Location = new System.Drawing.Point(0, 38);
+            this.txtPattern.Name = "txtPattern";
+            this.txtPattern.Size = new System.Drawing.Size(491, 26);
+            this.txtPattern.TabIndex = 1;
+            // 
+            // txtReplacement
+            // 
+            this.txtReplacement.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.txtReplacement.Location = new System.Drawing.Point(0, 38);
+            this.txtReplacement.Name = "txtReplacement";
+            this.txtReplacement.Size = new System.Drawing.Size(529, 26);
+            this.txtReplacement.TabIndex = 2;
+            // 
+            // btnRemoveRule
+            // 
+            this.btnRemoveRule.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnRemoveRule.Location = new System.Drawing.Point(17, 317);
+            this.btnRemoveRule.Name = "btnRemoveRule";
+            this.btnRemoveRule.Size = new System.Drawing.Size(113, 29);
+            this.btnRemoveRule.TabIndex = 3;
+            this.btnRemoveRule.Text = "Remove Rule";
+            this.btnRemoveRule.UseVisualStyleBackColor = true;
+            this.btnRemoveRule.Click += new System.EventHandler(this.btnRemoveRule_Click);
+            // 
+            // lstRules
+            // 
+            this.lstRules.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstRules.FormattingEnabled = true;
+            this.lstRules.ItemHeight = 20;
+            this.lstRules.Location = new System.Drawing.Point(16, 128);
+            this.lstRules.Name = "lstRules";
+            this.lstRules.Size = new System.Drawing.Size(1107, 164);
+            this.lstRules.TabIndex = 5;
+            this.lstRules.DoubleClick += new System.EventHandler(this.lstRules_DoubleClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Rules:";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(17, 12);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.txtPattern);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.label3);
+            this.splitContainer1.Panel2.Controls.Add(this.txtReplacement);
+            this.splitContainer1.Size = new System.Drawing.Size(1024, 64);
+            this.splitContainer1.SplitterDistance = 491;
+            this.splitContainer1.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Find";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Replace to";
+            // 
+            // MainForm
+            // 
+            this.AcceptButton = this.btnAddRule;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1146, 358);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lstRules);
+            this.Controls.Add(this.btnRemoveRule);
+            this.Controls.Add(this.btnAddRule);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAddRule;
+        private System.Windows.Forms.TextBox txtPattern;
+        private System.Windows.Forms.TextBox txtReplacement;
+        private System.Windows.Forms.Button btnRemoveRule;
+        private System.Windows.Forms.ListBox lstRules;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ PostClip/MainForm.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Windows.Forms;
+
+namespace PostClip
+{
+    public partial class MainForm : Form
+    {
+        private List<(string pattern, string replacement)> rules = new List<(string, string)>();
+        private string rulesFilePath = "rules.txt";
+
+        public MainForm()
+        {
+            InitializeComponent();
+            LoadRulesFromFile();
+            RegisterClipboardViewer();
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+            if (m.Msg == WM_CLIPBOARDUPDATE)
+            {
+                IDataObject iData = Clipboard.GetDataObject();
+                if (iData.GetDataPresent(DataFormats.Text))
+                {
+                    string text = (string)iData.GetData(DataFormats.Text);
+                    string replacedText = ApplyReplacementRules(text);
+                    if (replacedText != null && replacedText != text)
+                    {
+                        Clipboard.SetText(replacedText);
+                    }
+                }
+            }
+        }
+
+        private string ApplyReplacementRules(string text)
+        {
+            bool match = false;
+            foreach (var rule in rules)
+            {
+                text = Regex.Replace(text, rule.pattern, rule.replacement);
+                match = true;
+            }
+            if (!match)
+            {
+                return null;
+            }
+            return text;
+        }
+
+        private void btnAddRule_Click(object sender, EventArgs e)
+        {
+            string pattern = txtPattern.Text;
+            string replacement = txtReplacement.Text;
+            rules.Add((pattern, replacement));
+            UpdateRulesList();
+            SaveRulesToFile();
+        }
+
+        private void btnRemoveRule_Click(object sender, EventArgs e)
+        {
+            if (lstRules.SelectedIndex >= 0)
+            {
+                rules.RemoveAt(lstRules.SelectedIndex);
+                UpdateRulesList();
+                SaveRulesToFile();
+            }
+        }
+
+        private void UpdateRulesList()
+        {
+            lstRules.Items.Clear();
+            foreach (var rule in rules)
+            {
+                lstRules.Items.Add($"{rule.pattern} -> {rule.replacement}");
+            }
+        }
+
+        private void LoadRulesFromFile()
+        {
+            if (File.Exists(rulesFilePath))
+            {
+                string[] lines = File.ReadAllLines(rulesFilePath);
+                foreach (string line in lines)
+                {
+                    string[] parts = line.Split(new[] { "->" }, StringSplitOptions.RemoveEmptyEntries);
+                    if (parts.Length == 2)
+                    {
+                        rules.Add((parts[0].Trim(), parts[1].Trim()));
+                    }
+                }
+                UpdateRulesList();
+            }
+        }
+
+        private void SaveRulesToFile()
+        {
+            List<string> lines = new List<string>();
+            foreach (var rule in rules)
+            {
+                lines.Add($"{rule.pattern} -> {rule.replacement}");
+            }
+            File.WriteAllLines(rulesFilePath, lines);
+        }
+
+        private void RegisterClipboardViewer()
+        {
+            IntPtr hwnd = this.Handle;
+            WinAPI.AddClipboardFormatListener(hwnd);
+        }
+
+        private const int WM_CLIPBOARDUPDATE = 0x031D;
+
+        protected void DisposeCustom(bool disposing)
+        {
+            if (disposing)
+            {
+                WinAPI.RemoveClipboardFormatListener(this.Handle);
+            }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lstRules_DoubleClick(object sender, EventArgs e)
+        {
+            var msg = "Do you want to remove the selected rule?";
+            var title = "Remove Rule";
+            var result = MessageBox.Show(msg, title, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result != DialogResult.Yes)
+            {
+                return;
+            }
+
+            var sel = lstRules.SelectedItems;
+            if (sel.Count != 1)
+            {
+                return; 
+            }
+
+            var item = sel[0];
+            if (item == null)
+            {
+                return;
+            }
+
+            var parts = item.ToString().Split(new[] { "->" }, StringSplitOptions.RemoveEmptyEntries);
+            if (parts.Length != 2)
+            {
+                return;
+            }
+            txtPattern.Text = parts[0].Trim();
+            txtReplacement.Text = parts[1].Trim();
+
+            rules.RemoveAt(lstRules.SelectedIndex);
+            UpdateRulesList();
+        }
+    }
+
+    internal static class WinAPI
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool AddClipboardFormatListener(IntPtr hwnd);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool RemoveClipboardFormatListener(IntPtr hwnd);
+    }
+}
